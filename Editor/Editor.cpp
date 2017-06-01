6bb399d1023907a1f3b8ca82f901d50499298541
deadcast <deadcast@sent.com>
2017-05-31 21:46:36 -0700

    Added basic window creation code

---
+++ Editor/Editor.cpp
@@ -0,0 +1,71 @@
+#include <windows.h>
+#include <stdlib.h>
+#include <string.h>
+#include <tchar.h>
+
+static TCHAR szWindowClass[] = _T("UltraEd");
+static TCHAR szTitle[] = _T("UltraEd v0.1");
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+  switch(message)
+  {
+  case WM_DESTROY:
+    PostQuitMessage(0);
+    break;
+  default:
+    return DefWindowProc(hWnd, message, wParam, lParam);
+    break;
+  }
+  
+  return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                   LPSTR lpCmdLine, int nCmdShow)
+{
+  WNDCLASSEX wcex;
+  wcex.cbSize = sizeof(WNDCLASSEX);
+  wcex.style = CS_HREDRAW | CS_VREDRAW;
+  wcex.lpfnWndProc = WndProc;
+  wcex.cbClsExtra = 0;
+  wcex.cbWndExtra = 0;
+  wcex.hInstance = hInstance;
+  wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+  wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+  wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+  wcex.lpszMenuName = NULL;
+  wcex.lpszClassName = szWindowClass;
+  wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+  
+  if(!RegisterClassEx(&wcex))
+  {
+    MessageBox(NULL, _T("Call to RegisterClassEx failed!"),
+      _T("Error"), NULL);
+    
+    return 1;
+  }
+  
+  HWND hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+    CW_USEDEFAULT, CW_USEDEFAULT, 800, 600, NULL, NULL, hInstance, NULL);
+  
+  if(!hWnd)
+  {
+    MessageBox(NULL, _T("Call to CreateWindow failed!"),
+      _T("Error"), NULL);
+    
+    return 1;
+  }
+  
+  ShowWindow(hWnd, nCmdShow);
+  UpdateWindow(hWnd);
+  
+  MSG msg;
+  while (GetMessage(&msg, NULL, 0, 0))
+  {
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
+  }
+  
+  return msg.wParam;
+}
