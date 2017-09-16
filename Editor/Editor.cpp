d020443386b627d27a15e29435bf314912714c4c
deadcast <deadcast@sent.com>
2017-09-15 21:34:33 -0700

    Added titles to toolbar buttons

--- Editor/Editor.cpp
+++ Editor/Editor.cpp
@@ -196,19 +196,19 @@ HWND CreateToolbar(HWND hWnd, HINSTANCE hInst)
   tbrButtons[0].fsState   = TBSTATE_ENABLED;
   tbrButtons[0].fsStyle   = TBSTYLE_BUTTON;
   tbrButtons[0].iBitmap   = 0;
-  tbrButtons[0].iString   = 0;
+  tbrButtons[0].iString   = (INT_PTR)L"Move";
   
   tbrButtons[1].idCommand = IDM_TOOLBAR_ROTATE;
   tbrButtons[1].fsState   = TBSTATE_ENABLED;
   tbrButtons[1].fsStyle   = TBSTYLE_BUTTON;
   tbrButtons[1].iBitmap   = 1;
-  tbrButtons[1].iString   = 0;
+  tbrButtons[1].iString   = (INT_PTR)L"Rotate";
   
   tbrButtons[2].idCommand = IDM_TOOLBAR_SCALE;
   tbrButtons[2].fsState   = TBSTATE_ENABLED;
   tbrButtons[2].fsStyle   = TBSTYLE_BUTTON;
   tbrButtons[2].iBitmap   = 2;
-  tbrButtons[2].iString   = 0;
+  tbrButtons[2].iString   = (INT_PTR)L"Scale";
 
   tbrButtons[3].fsState   = TBSTATE_ENABLED;
   tbrButtons[3].fsStyle   = TBSTYLE_SEP;
@@ -217,28 +217,28 @@ HWND CreateToolbar(HWND hWnd, HINSTANCE hInst)
   tbrButtons[4].fsState   = TBSTATE_ENABLED;
   tbrButtons[4].fsStyle   = TBSTYLE_BUTTON;
   tbrButtons[4].iBitmap   = 3;
-  tbrButtons[4].iString   = 0;
+  tbrButtons[4].iString   = (INT_PTR)L"Persp.";
 
   tbrButtons[5].idCommand = IDM_TOOLBAR_VIEW_TOP;
   tbrButtons[5].fsState   = TBSTATE_ENABLED;
   tbrButtons[5].fsStyle   = TBSTYLE_BUTTON;
   tbrButtons[5].iBitmap   = 4;
-  tbrButtons[5].iString   = 0;
+  tbrButtons[5].iString   = (INT_PTR)L"Top";
 
   tbrButtons[6].idCommand = IDM_TOOLBAR_VIEW_LEFT;
   tbrButtons[6].fsState   = TBSTATE_ENABLED;
   tbrButtons[6].fsStyle   = TBSTYLE_BUTTON;
   tbrButtons[6].iBitmap   = 5;
-  tbrButtons[6].iString   = 0;
+  tbrButtons[6].iString   = (INT_PTR)L"Left";
 
   tbrButtons[7].idCommand = IDM_TOOLBAR_VIEW_FRONT;
   tbrButtons[7].fsState   = TBSTATE_ENABLED;
   tbrButtons[7].fsStyle   = TBSTYLE_BUTTON;
   tbrButtons[7].iBitmap   = 6;
-  tbrButtons[7].iString   = 0;
+  tbrButtons[7].iString   = (INT_PTR)L"Front";
   
   return CreateToolbarEx(hWnd,
-    WS_VISIBLE | WS_CHILD | WS_BORDER,
+    WS_VISIBLE | WS_CHILD | WS_BORDER | TBSTYLE_TOOLTIPS,
     IDB_TOOLBAR,
     8,
     hInst,
@@ -290,7 +290,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     MessageBox(NULL, "Could not create toolbar.", "Error", NULL);
     return 1;
   }
-  
+
   ShowWindow(parentWindow, nCmdShow);
   UpdateWindow(parentWindow);
   
