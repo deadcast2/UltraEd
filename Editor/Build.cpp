227420ac3fa8af100fcd92202e9ba3c239a1c52e
Caleb Cohoon <caleb@imap.cc>
2021-09-06 21:17:29 -0500

    Fixed warning for actor file generation.

--- Editor/Build.cpp
+++ Editor/Build.cpp
@@ -206,7 +206,7 @@ namespace UltraEd
         std::string actorsArrayDef("vector _UER_Actors = NULL;\nvector _UER_ActorsPendingRemoval = NULL;");
         actorsArrayDef.append("\nActor *_UER_ActiveCamera = NULL;\n");
 
-        std::string actorInits("\n\t_UER_Actors = vector_create();\_UER_ActorsPendingRemoval = vector_create();\n");
+        std::string actorInits("\n\t_UER_Actors = vector_create();\n\t_UER_ActorsPendingRemoval = vector_create();\n");
 
         std::map<boost::uuids::uuid, int> reducedActorIds;
         
